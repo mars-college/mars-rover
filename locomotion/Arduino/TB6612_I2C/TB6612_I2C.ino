e7e752b6022c9f5af6e333df2b747f715eff0061
phillipdavidstearns <phil@phillipstearns.com>
2020-06-08 11:37:23 -0600

    08June2020

--- locomotion/Arduino/TB6612_I2C/TB6612_I2C.ino
+++ locomotion/Arduino/TB6612_I2C/TB6612_I2C.ino
@@ -1,4 +1,6 @@
+#include <Wire.h>
 #include <TimerOne.h>
+
 
 #define PWMA 3
 #define PWMB 11
@@ -23,7 +25,8 @@ float scale = 0.0;
 
 
 void setup() {
-
+  Wire.begin(8);                // join i2c bus with address #8
+  Wire.onReceive(receiveEvent); // register event
   Serial.begin(115200);
   // TimerOne Setup
   // for regular sampling of speed
@@ -51,6 +54,15 @@ void loop() {
   analogWrite(PWMA, sin2Scaled(progress));
   analogWrite(PWMB, sin2Scaled(progress));
 
+}
+
+void receiveEvent(int howMany) {
+  while (1 < Wire.available()) { // loop through all but the last
+    char c = Wire.read(); // receive byte as a character
+    Serial.print(c);         // print the character
+  }
+  int x = Wire.read();    // receive byte as an integer
+  Serial.println(x);         // print the integer
 }
 
 void randomize(){
