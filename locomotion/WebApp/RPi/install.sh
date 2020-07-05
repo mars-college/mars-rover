4d9bd15b13d7cad2a16b1648d0bef42fc6dc640b
phillipdavidstearns <phil@phillipstearns.com>
2020-07-04 22:39:45 -0600

    update to un/install and update scripts for RPi WebApp

--- locomotion/WebApp/RPi/install.sh
+++ locomotion/WebApp/RPi/install.sh
@@ -5,7 +5,7 @@ sudo mkdir /usr/local/bin/rover
 sudo cp server.py /usr/local/bin/rover/
 sudo cp -r templates /usr/local/bin/rover/
 sudo cp -r static /usr/local/bin/rover/
-sudo cp rover.service /etc/systemd/system/rover.service
+sudo cp rover.service /lib/systemd/system/rover.service
 sudo systemctl daemon-reload
 sudo systemctl start rover.service
 systemctl status rover.service
