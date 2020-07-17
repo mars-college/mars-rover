7343479e8055575e151c8b1b6bb6c77d18816137
phillipdavidstearns <phil@phillipstearns.com>
2020-07-17 02:21:13 -0600

    tinkering with the GStreamer pipe

--- vision/flask/README.md
+++ vision/flask/README.md
@@ -1,8 +1,9 @@
 # Example of serving up PiCam in a web browser on JetsonNano using Flask
 
-* [Using Jetson Nano and a Raspberry Pi Camera for Video Streaming](https://maker.pro/nvidia-jetson/tutorial/streaming-real-time-video-from-rpi-camera-to-browser-on-jetson-nano-with-flask)
+* Code lifted from: [Using Jetson Nano and a Raspberry Pi Camera for Video Streaming](https://maker.pro/nvidia-jetson/tutorial/streaming-real-time-video-from-rpi-camera-to-browser-on-jetson-nano-with-flask)
 
 1. Install dependencies `$ sudo apt-get install python3-opencv python3-flask`
 1. Run the server: `$ python3 server.py`
+1. Open your browser and point it to: `http://<your_nano_IP>:8080`
 
-Doesn't work... no errors...
+* more nvidia GStreamer stuff [here](https://docs.nvidia.com/jetson/archives/l4t-archived/l4t-3231/index.html#page/Tegra%2520Linux%2520Driver%2520Package%2520Development%2520Guide%2Faccelerated_gstreamer.html%23wwpID0E0OQ0HA)
--- vision/flask/server.py
+++ vision/flask/server.py
@@ -13,7 +13,7 @@ thread_lock = threading.Lock()
 
 # GStreamer Pipeline to access the Raspberry Pi camera
 # GSTREAMER_PIPELINE = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3280, height=2464, format=(string)NV12, framerate=21/1 ! nvvidconv flip-method=0 ! video/x-raw, width=960, height=616, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink wait-on-eos=false max-buffers=1 drop=True'
-GSTREAMER_PIPELINE = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3280, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method=0 ! videoconvert ! video/x-raw, format=(string)BGR ! appsink wait-on-eos=false max-buffers=1 drop=True'
+GSTREAMER_PIPELINE = 'nvarguscamerasrc sensor_id=0 ! video/x-raw(memory:NVMM), width=3280, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method=0 ! videoconvert ! video/x-raw, format=(string)BGR ! appsink wait-on-eos=false max-buffers=1 drop=True'
 
 
 # Create the Flask object for the application
