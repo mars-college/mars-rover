6b656356e91129b36535a774cb7ab24cce1af870
phillipdavidstearns <phil@phillipstearns.com>
2020-08-29 04:45:07 -0700

    zero power to motors on socket close

--- locomotion/WebApp/JetsonNano/server.py
+++ locomotion/WebApp/JetsonNano/server.py
@@ -9,31 +9,45 @@ from smbus import SMBus
 from time import sleep
 import math
 
-left_direction = True
-right_direction = True
-left_direction_last = True
-right_direction_last = True
+# Tornado resource paths
+settings = dict(
+	template_path = os.path.join(os.path.dirname(__file__), "templates"),
+	static_path = os.path.join(os.path.dirname(__file__), "static")
+	)
 
-ADDRESSES = [ 0x10 , 0x11 ]
+# Tonado server port
+PORT = 80
 
 bus = SMBus(1)
+ADDRESSES = [ 0x10 , 0x11 ]
 
-def send_i2c_data(address, data):
-  try:
-    bus.write_i2c_block_data( address, 0, data )
-  except Exception as e:
-      print(e)
-      pass
+left_direction_last = True
+right_direction_last = True 
+
+def init_motors():
+    # set direction to forward
+    send_i2c_data( ADDRESSES[0], [ ord('d'), int(True) ] )
+    send_i2c_data( ADDRESSES[1], [ ord('d'), int(True) ] )
+    # set power to 0
+    send_i2c_data( ADDRESSES[0], [ ord('p'), 0 ] )
+    send_i2c_data( ADDRESSES[1], [ ord('p'), 0 ] )
 
 def set_power(drive, turn):
 
-  if drive < 0:
+  # constrain drive and turn to -1.0 to 1.0
+  # scale by 256
+  drive = constrain( drive, -1.0 ,1.0) * 256
+  turn = constrain( turn, -1.0 ,1.0) * 256
+
+  # cacluate power
+  if drive <= 0:
     left_power = drive + turn
     right_power = drive - turn
   else:
     left_power = drive - turn
     right_power = drive + turn
 
+  # set direction based on power
   if left_power >= 0:
     left_direction = True
   else:
@@ -44,49 +58,51 @@ def set_power(drive, turn):
   else:
     right_direction = False
 
-  if left_direction == left_direction_last:
+  # send direction changes only (not sure if this is entirely necessary)
+  if left_direction != left_direction_last:
     send_i2c_data( ADDRESSES[0], [ ord('d'), int(left_direction) ] )
 
-
-  if right_direction == right_direction_last:
+  if right_direction != right_direction_last:
     send_i2c_data( ADDRESSES[1], [ ord('d'), int(right_direction) ] )
 
-
+  # absolute value constrained to integer values 0-255
   left_power = int(min(abs(left_power), 255))
   right_power = int(min(abs(right_power), 255))
   
   send_i2c_data(ADDRESSES[0], [ ord('p'), left_power ] )
   send_i2c_data(ADDRESSES[1], [ ord('p'), right_power ] )
 
-
   return left_direction, right_direction
 
-settings = dict(
-	template_path = os.path.join(os.path.dirname(__file__), "templates"),
-	static_path = os.path.join(os.path.dirname(__file__), "static")
-	)
+def constrain( _val, _min, _max):
+  return min(_max, max(_min,_val))
 
-#Tonado server port
-PORT = 80
-
+def send_i2c_data(address, data):
+  try:
+    bus.write_i2c_block_data( address, 0, data )
+  except Exception as e:
+    print(e)
+    pass
 
 class MainHandler(tornado.web.RequestHandler):
   def get(self):
      print ("[HTTP](MainHandler) User Connected.")
      self.render("index.html")
 
-	
-class WSHandler(tornado.websocket.WebSocketHandler):
+class WSHandler(tornado.websocket.WebSocketHandler): 
+
   def open(self):
     print ('[WS] Connection was opened.')
- 
+
+  def on_close(self):
+    # stop motors
+    set_power( 0.0 , 0.0 )
+    print ('[WS] Connection was closed.')
+
   def on_message(self, message):
 
     global left_direction_last
-    global right_direction_last
-
-    drive = 0
-    turn = 0
+    global right_direction_last 
 
     # print ('[WS] Incoming message:', message)
 
@@ -94,17 +110,17 @@ class WSHandler(tornado.websocket.WebSocketHandler):
     # print ('[WS] Incoming message:', message)
 
     if message[0] == 'j':
-      drive = int(256 * float(message[1]))
-      turn = int(256 * float(message[2]))
-      # print("Drive: "+str(drive)+"Turn: "+str(turn))
+      drive = float(message[1])
+      turn = float(message[2])
       left_direction_last, right_direction_last = set_power( drive , turn )
 
-    if message[0] == "on_g":
-        send_i2c_data(ADDRESSES[0], [ ord('b'), 255 ] )
-        send_i2c_data(ADDRESSES[1], [ ord('b'), 255 ] )
-    if message[0] == "off_g":
-        send_i2c_data(ADDRESSES[0], [ ord('b'), 0 ] )
-        send_i2c_data(ADDRESSES[1], [ ord('b'), 0 ] )
+    # Button Messages
+    # if message[0] == "on_g":
+    #     send_i2c_data(ADDRESSES[0], [ ord('b'), 255 ] )
+    #     send_i2c_data(ADDRESSES[1], [ ord('b'), 255 ] )
+    # if message[0] == "off_g":
+    #     send_i2c_data(ADDRESSES[0], [ ord('b'), 0 ] )
+    #     send_i2c_data(ADDRESSES[1], [ ord('b'), 0 ] )
       
     # if message == "on_r":
     #   GPIO.output(18, True)
@@ -120,9 +136,6 @@ class WSHandler(tornado.websocket.WebSocketHandler):
     #   GPIO.output(13 , True)
     # if message == 'off_w':
     #   GPIO.output(13 , False)
-
-  def on_close(self):
-    print ('[WS] Connection was closed.')
 
 application = tornado.web.Application([
   (r'/', MainHandler),
